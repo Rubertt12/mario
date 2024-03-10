ed7b558274641bfeace16364204ca3c1bc4fc0ec
rubertt12 <rubertramires@hotmail.com>
2024-03-10 00:03:03 -0300

    novo ajuste com modal

--- js/script.js
+++ js/script.js
@@ -16,15 +16,55 @@ const jump = () => {
             isJumping = false;
         }, 500);
     }
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Esconde o botão no início
+    document.getElementById('botaoContainer').style.display = 'none';
+});
+
+function restartGame() {
+    // Recarrega a página
+    location.reload();
 }
+
+const showGameOverModal = () => {
+    // Exibe o modal de reinício
+    const modal = document.createElement('div');
+    modal.className = 'modal';
+
+    const gameOverText = document.createElement('p');
+    gameOverText.textContent = 'Game Over';
+
+    const jumpCountText = document.createElement('p');
+    jumpCountText.textContent = `Total Jumps: ${jumpCounter}`;
+
+    const restartButton = document.createElement('button');
+    restartButton.textContent = 'Reiniciar';
+    restartButton.className = 'botao';
+    restartButton.addEventListener('click', restartGame);
+
+    const botaoContainer = document.createElement('div');
+    botaoContainer.className = 'botao-container';
+    botaoContainer.appendChild(restartButton);
+
+    modal.appendChild(gameOverText);
+    modal.appendChild(jumpCountText);
+    modal.appendChild(botaoContainer);
+
+    document.body.appendChild(modal);
+
+    // Exibe o modal de reinício
+    modal.style.display = 'block';
+};
 
 const handleTouch = (event) => {
     jump();
-}
+};
 
 const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = window.getComputedStyle(mario).bottom.replace('px', '');
+    const marioPosition = parseFloat(window.getComputedStyle(mario).bottom);
 
     if (pipePosition < 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = 'none';
@@ -35,9 +75,12 @@ const loop = setInterval(() => {
 
         mario.src = '/images/game-over.png';
         mario.style.width = '75px';
-        mario.style.marginLeft = '50px';
+        mario.style.marginLeft = '60px';
 
         clearInterval(loop);
+
+        // Chama a função para mostrar o modal quando o jogador morre
+        showGameOverModal();
     }
 }, 10);
 
